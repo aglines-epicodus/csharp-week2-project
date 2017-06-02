d25461f2cb645e430c4496150d29f3c4cb99628c
aglines-epicodus <aglines.epicodus@gmail.com>
2017-06-02 10:26:16 -0700

    add test for multiple count input, fail test

--- Tests/CountTests.cs
+++ Tests/CountTests.cs
@@ -6,12 +6,13 @@ namespace Wordcount
 {
   public class WordcountTests
   {
+    // | Input  "a", "a" | Output 1 |
     [Fact]
     public void CountRepeats_SingleWordInput_Returns1()
     {
       //Arrange
-      string testInputWord = "a";
       string testInputString = "a";
+      string testInputWord = "a";
       int expectedCount = 1;
       //Act
       int result = RepeatCounter.CountRepeats(testInputWord, testInputString);
@@ -19,12 +20,24 @@ namespace Wordcount
       Assert.Equal(expectedCount, result);
     }
 
+    // | Input  "A", "a" | Output 1 |
     [Fact]
     public void CountRepeats_UpperCaseInput_Returns1()
     {
-      string testInputWord = "a";
       string testInputString = "A";
+      string testInputWord = "a";
       int expectedCount = 1;
+      int result = RepeatCounter.CountRepeats(testInputWord, testInputString);
+      Assert.Equal(expectedCount, result);
+    }
+
+    // | Input  "a a", "a" | Output 2 |
+    [Fact]
+    public void CountRepeats_TwoExamplesInput_Returns2()
+    {
+      string testInputString = "a a";
+      string testInputWord = "a";
+      int expectedCount = 2;
       int result = RepeatCounter.CountRepeats(testInputWord, testInputString);
       Assert.Equal(expectedCount, result);
     }
